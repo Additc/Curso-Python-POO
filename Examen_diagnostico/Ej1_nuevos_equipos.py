10983c9e9e973466f2ee94d256502c5e0205c2a3
Addilap <chavezaddi12@gmail.com>
2025-03-04 18:11:37 -0600

    ejercicio

--- Examen_diagnostico/Ej1_nuevos_equipos.py
+++ Examen_diagnostico/Ej1_nuevos_equipos.py
@@ -18,23 +18,42 @@ pero con la restricción de que no puede haber dos personas que ya estuvieron en
 from random import choice
 import random
 
-algortimos=["Hector","Alberto", "Addi"]
-hackers=["Tania","Patricia", "Rebeca"]
-codificadores=["Jamileth","Bryan", "Rosalinda"]
-ctrl=["Galilea","Jennifer", "Juan"]
-alumnos=["Galilea","Jennifer", "Juan","Jamileth","Bryan", "Rosalinda","Tania","Patricia", "Rebeca","Hector","Alberto", "Addi"]
+def menu():
+    print("Menú de opciones")
+    print("1) Generar equipos")
+    print("2) salir")
+    opcion=input("Ingrese una opción a realizar: ")
+    while not opcion.isnumeric():
+        print("Opción no válida")
+        opcion = input("Intenta nuevamente: ")
+    opcion = int(opcion)
+    print()
+    return opcion
 
-equipo1=[]
-equipo2=[]
-equipo3=[]
-equipo4=[]
-equipo5=[]
-equipo6=[]
 
-contador=1
 
-for i in range (0,6):
-    alumno1= random.choice(alumnos)
-    alumno2= random.choice(alumnos)
-    print(f"El equipo {contador} es {alumno1,alumno2}")
-    contador+=1
+def armar_equipos(algoritmos,hackers,codificadores, ctrl, alumnos):
+    alumno1 = random.choice(alumnos)
+    alumno2 = random.choice(alumnos)
+
+
+
+
+def menu_p()->None:
+    algoritmos=["Hector","Alberto", "Addi"]
+    hackers=["Tania","Patricia", "Rebeca"]
+    codificadores=["Jamileth","Bryan", "Rosalinda"]
+    ctrl=["Galilea","Jennifer", "Juan"]
+    alumnos=["Galilea","Jennifer", "Juan","Jamileth","Bryan", "Rosalinda","Tania","Patricia", "Rebeca","Hector","Alberto", "Addi"]
+    while True:
+        opcion = menu()
+        if opcion == 1:
+            armar_equipos(algoritmos,hackers,codificadores, ctrl, alumnos )
+        elif opcion == 0:
+            print("Vuelve pronto.")
+            break
+        else:
+            print("Valor no válido.")
+
+if __name__ == '__main__':
+    menu_p()
