881f51a9bfaf39142eefeba5ea0d688ee2278a0b
AddiSC2 <chavezaddi12@gmail.com>
2025-03-10 16:01:42 -0600

    clases y objetos

--- clases-y-objetos/atributos_de_instancia.py
+++ clases-y-objetos/atributos_de_instancia.py
@@ -7,15 +7,41 @@ class Estudiante:
         self.temas_aprendidos.append(tema) # aniadimos un tema a lista de ese objeto
         print(f"{self.nombre} aprendió {tema}")
 
+    #Método mágico
+    def __str__(self)->str:
+        return f"Estudiante nombre:{self.nombre} Temas aprendidos: {self.temas_aprendidos} "
+
 class Profesor:
-    def __init__(self, nombre: str):
+    def __init__(self, nombre: str,temas_dominados:list[str]):
         self.nombre = nombre  # Hacemos que forme parte del objeto
-        self.temas_dominados =[]
+        self.temas_dominados = temas_dominados
 
     def dominar_tema(self,tema: str)-> None:
         self.temas_dominados.append(tema)  # aniadimos un tema a lista de ese objeto
         print(f"EL profesor {self.nombre} domina el tema: {tema}")
 
     def enseniar_tema(self,no_tema:int)->str:
-        tema_enseniar=self.temas_dominados[no_tema]
-        return tema_enseniar
+        if no_tema> len(self.temas_dominados): #Válidamos temas
+            return self.temas_dominados[no_tema]
+        else:
+            return "Fuera de rango"
+
+    # Método mágico
+    def __str__(self) -> str:
+        return f"Profesor nombre:{self.nombre} Temas dominados: {self.temas_dominados} "
+
+
+if __name__=="__main__":
+    estudiante1=Estudiante("Addi")
+    estudiante2=Estudiante("Alberto")
+
+    estudiante1.aprender_tema("Evolución sitios web")
+    estudiante2.aprender_tema("Internet de las cosas")
+    print()
+    print(estudiante1)
+    print(estudiante2)
+    print()
+
+    #Ahora para la class profesor
+    profesor1=Profesor("Alberto",["matemáticas","Programación"])
+    print(profesor1)
