fa6acf849cff883c7e0706ccd659b6f47a4d7dff
AddiSC2 <chavezaddi12@gmail.com>
2025-03-12 16:01:05 -0600

    clases y objetos

---
+++ clases-y-objetos/Ejercicio1_Personaje.py
@@ -0,0 +1,40 @@
+"""
+Nombre: Addi Toro Chávez.
+Fecha: 12 de Marzo del 2025
+Descripción:
+Ejercicio 1 clase personaje.
+"""
+
+class Personaje:
+    """
+    Clase que representa un personaje.
+    Sus atributos son: no_id (atributo de clase).
+    Sus métodos son: __init__(), __str__(), moverse(), pocision_actual().
+    """
+    no_id = 1
+
+
+    def __init__(self)->None:
+        """
+        Comstructor del Personaje
+        """
+
+        self.x = x=0
+        self.y = y=0
+        self.id_personaje = Personaje.no_id
+        Personaje.no_id += 1
+
+    def moverse(self,ordenes:str)->None:
+        pass
+
+    def pocision_actual(self)->None:
+        pass
+
+    def __str__(self) -> str:
+        return f"Profesor nombre: "
+
+
+
+""" %%%%%%%     CÓDIGO A NIVEL DE MÓDULO    %%%%%%%%%%%%%%%%%%%%% """
+if __name__ == "__main__":
+    pass
