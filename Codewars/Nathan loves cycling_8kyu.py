90cbd6e60d21062c3c51aa10cb10b0676e792855
AddiSC2 <chavezaddi12@gmail.com>
2025-03-18 15:54:38 -0600

    codewars

---
+++ Codewars/Nathan loves cycling_8kyu.py
@@ -0,0 +1,21 @@
+"""
+Nombre: Addi Toro Chávez.
+Fecha: 12 de marzo del 2025
+Descripción:
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded down.
+"""
+
+
+def litres(time):
+    litros=0.5*time
+    return int(litros)
+
+
+""" %%%%%%%     CÓDIGO A NIVEL DE MÓDULO    %%%%%%%%%%%%%%%%%%%%% """
+if __name__ == '__main__':
+    print(litres(0))
+    print(litres(1))
+    print(litres(2))
+    print(litres(3))
+    print(litres(4))
